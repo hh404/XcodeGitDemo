ba466b876ee16d40e0709d80942f70be35cd23bb
Hans <huangjianwu@chinasofti.com>
2018-07-07 11:27:26 +0800

    # feat(<feature2): add new feature named feature2

--- XcodeGitDemo/ViewController.swift
+++ XcodeGitDemo/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // deveop branch is root branch
         
+        
+        //add  function named feature2
+        
     }
 
     override func didReceiveMemoryWarning() {
